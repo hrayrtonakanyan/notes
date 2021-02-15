83bf8f7d26c35dc6924168487d32d341f2d6064f
hrayr <hrayr.tonakanyan@gmail.com>
2021-02-15 23:21:03 +0400

    Initial commit

---
+++ src/test/java/com/disqo/assessment/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disqo.assessment.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
